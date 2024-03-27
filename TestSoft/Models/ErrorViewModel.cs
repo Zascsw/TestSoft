839861f0f075e136da8e8df5b906529ab878763d
Deleu <98638420+Zascsw@users.noreply.github.com>
2024-03-27 16:40:31 +0200

    Delete TestSoft directory

--- TestSoft/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TestSoft.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
