e5b768c8e117393c6fc7b9ea38e2ee6e8f544d8d
Vlad <vdeleu6@gmail.com>
2024-03-31 22:55:06 +0300

    Create1.0

---
+++ TestSoft/Models/TableModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class TableModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Введите имя таблицы")]
+    public string TableName { get; set; }
+
+    [Required(ErrorMessage = "Введите названия столбцов (через запятую)")]
+    public string Columns { get; set; }
+}
